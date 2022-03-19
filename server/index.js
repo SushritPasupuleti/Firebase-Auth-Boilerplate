98b13d147aea64bd9c4c01a2f3d6489b8a12fc1e
Sushrit_Lawliet <sushrit.pk21@gmail.com>
2022-03-19 18:42:11 +0530

    feat: add cors and helmet

--- server/index.js
+++ server/index.js
@@ -1,14 +1,21 @@
+require('dotenv').config()
+const cors = require('cors');
 const express = require('express')
 const { default: helmet } = require('helmet')
 const app = express()
-const port = 3000
+const port = 5000
 
 app.use(helmet)
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
 
 app.listen(port, () => {
-  console.log(`API Server listening on port ${port}`)
+    console.log(`API Server listening on port ${port}`)
 })
